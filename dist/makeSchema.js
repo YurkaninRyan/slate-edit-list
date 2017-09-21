21fe7fed4cf18e247102fe957d4397ca89f1cfdc
Ryan Yurkanin <ryurkanin@getguru.com>
2017-09-21 09:08:39 -0400

    building

--- dist/makeSchema.js
+++ dist/makeSchema.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 var isList = require('./isList');
 
 /**
@@ -13,7 +11,7 @@ function makeSchema(opts) {
     return {
         rules: [listsContainOnlyItems(opts), itemsDescendList(opts),
         // Must be after itemsDescendList
-        itemsContainBlocks(opts), joinAdjacentLists(opts)]
+        itemsContainBlocks(opts)]
     };
 }
 
@@ -130,51 +128,6 @@ function itemsContainBlocks(opts) {
             // Add the remaining items
             node.nodes.rest().forEach(function (child, index) {
                 return change.moveNodeByKey(child.key, wrapper.key, index + 1, noNorm);
-            });
-
-            return change;
-        }
-    };
-}
-
-/**
- * @param {PluginOptions} The plugin options
- * @return {Object} A rule that joins adjacent, same types lists
- */
-function joinAdjacentLists(opts) {
-    return {
-        match: function match(node) {
-            return node.kind === 'document' || node.kind === 'block';
-        },
-
-        validate: function validate(node) {
-            var invalids = node.nodes.map(function (child, i) {
-                if (!isList(opts, child)) return;
-                var next = node.nodes.get(i + 1);
-                if (!next || next.type !== child.type) return;
-                return [child, next];
-            }).filter(Boolean);
-
-            return invalids.size ? invalids : null;
-        },
-
-
-        /**
-         * Join the list pairs
-         */
-        normalize: function normalize(change, node, pairs) {
-            // We join in reverse order, so that multiple lists folds onto the first one
-            pairs.reverse().forEach(function (pair) {
-                var _pair = _slicedToArray(pair, 2),
-                    first = _pair[0],
-                    second = _pair[1];
-
-                var updatedSecond = change.state.document.getDescendant(second.key);
-                updatedSecond.nodes.forEach(function (secondNode, index) {
-                    change.insertNodeByKey(first.key, first.nodes.size + index, secondNode);
-                });
-
-                change.removeNodeByKey(second.key);
             });
 
             return change;
