4a4b9babc8652a0a147ae6bb6a7fb5a9b6cfb0bb
Ivana Veliskova <iveliskova@getguru.com>
2019-12-14 20:16:37 -0500

    Updates dist

--- dist/changes/wrapInList.js
+++ dist/changes/wrapInList.js
@@ -4,17 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _set = require("babel-runtime/core-js/set");
-
-var _set2 = _interopRequireDefault(_set);
-
 var _slate = require("slate");
 
 var _immutable = require("immutable");
 
 var _utils = require("../utils");
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
  * Wrap the blocks in the current selection in a new list. Selected
@@ -24,29 +18,14 @@ function wrapInList(opts, change, type, data) {
   var selectedBlocks = getHighestSelectedBlocks(change.value);
   type = type || opts.types[0];
 
-  var wrapper = _slate.Block.create({
+  // Wrap in container
+  change.wrapBlock({
     type: type,
     data: _slate.Data.create(data)
-  });
-
-  var topLevelIndex = change.value.document.nodes.findIndex(function (node) {
-    return node.key === selectedBlocks.get(0).key;
-  });
-  change.insertNodeByKey(change.value.document.key, topLevelIndex, wrapper, {
-    normalize: false
-  });
-  selectedBlocks.forEach(function (block) {
-    if (!change.value.document.getDescendant(block.key)) return;
-    change.removeNodeByKey(block.key, { normalize: false });
-  });
-
-  selectedBlocks.forEach(function (block, index) {
-    return change.insertNodeByKey(wrapper.key, index, block, { normalize: false });
-  });
+  }, { normalize: false });
 
   // Wrap in list items
-  wrapper = change.value.document.getDescendant(wrapper.key);
-  wrapper.nodes.forEach(function (node) {
+  selectedBlocks.forEach(function (node) {
     if ((0, _utils.isList)(opts, node)) {
       // Merge its items with the created list
       node.nodes.forEach(function (_ref) {
@@ -54,13 +33,9 @@ function wrapInList(opts, change, type, data) {
         return change.unwrapNodeByKey(key, { normalize: false });
       });
     } else {
-      if (node.object === "text") {
-        if (!change.value.document.getDescendant(node.key)) {
-          return;
-        }
-        return change.removeTextByKey(node.key);
-      }
-      change.wrapBlockByKey(node.key, opts.typeItem);
+      change.wrapBlockByKey(node.key, opts.typeItem, {
+        normalize: false
+      });
     }
   });
 
@@ -71,10 +46,22 @@ function wrapInList(opts, change, type, data) {
  * Returns the highest list of blocks that cover the current selection
  */
 
-function getHighestSelectedBlocks(state) {
-  return (0, _immutable.List)(new _set2.default(state.blocks.map(function (block) {
-    return state.document.getFurthestAncestor(block.key);
-  })));
+function getHighestSelectedBlocks(value) {
+  var range = value.selection;
+  var document = value.document;
+
+
+  var startBlock = document.getClosestBlock(range.startKey);
+  var endBlock = document.getClosestBlock(range.endKey);
+
+  if (startBlock === endBlock) {
+    return (0, _immutable.List)([startBlock]);
+  }
+  var ancestor = document.getCommonAncestor(startBlock.key, endBlock.key);
+  var startPath = ancestor.getPath(startBlock.key);
+  var endPath = ancestor.getPath(endBlock.key);
+
+  return ancestor.nodes.slice(startPath[0], endPath[0] + 1);
 }
 
 exports.default = wrapInList;
